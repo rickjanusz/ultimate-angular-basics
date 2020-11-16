e71340449bac463a5fb72c8682ed22499d61d80a
Rick Janusz <rjanusz@conversantmedia.com>
2020-11-16 15:38:15 -0600

    adding dynamic routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,39 @@
 import { Component } from '@angular/core'
 
+interface Nav {
+  link: string
+  name: string
+  exact: boolean
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: /*html */ ` <div class="app">
     <nav class="nav">
       <a
-        routerLink="/"
+        *ngFor="let item of nav"
+        [routerLink]="item.link"
         routerLinkActive="active"
-        [routerLinkActiveOptions]="{ exact: true }"
-        >Home</a
+        [routerLinkActiveOptions]="{ exact: item.exact }"
       >
-      <a routerLink="/oops" routerLinkActive="active">404</a>
+        {{ item.name }}</a
+      >
     </nav>
     <router-outlet></router-outlet>
   </div>`,
 })
-export class AppComponent {}
+export class AppComponent {
+  nav: Nav[] = [
+    {
+      link: '/',
+      name: 'Home',
+      exact: true,
+    },
+    {
+      link: '/oops',
+      name: '404',
+      exact: false,
+    },
+  ]
+}
