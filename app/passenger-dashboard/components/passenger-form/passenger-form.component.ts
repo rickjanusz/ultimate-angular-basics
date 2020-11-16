cea5757c2e44f6b2f36862b06713daaba92047ae
Rick Janusz <rjanusz@conversantmedia.com>
2020-11-16 13:39:20 -0600

    adding passenger-form.component continued / validation

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -10,12 +10,30 @@ import { Baggage } from '../../models/baggage.interface'
 
     <div>
       Passenger Name:
-      <input type="text" name="fullname" [ngModel]="detail?.fullname" />
+      <input
+        type="text"
+        name="fullname"
+        #fullname="ngModel"
+        required
+        [ngModel]="detail?.fullname"
+      />
+      <div *ngIf="fullname.errors?.required && fullname.dirty" class="error">
+        Passenger name is required
+      </div>
     </div>
 
     <div>
       Passenger ID:
-      <input type="number" name="id" [ngModel]="detail?.id" />
+      <input
+        type="number"
+        name="id"
+        required
+        #id="ngModel"
+        [ngModel]="detail?.id"
+      />
+      <div *ngIf="id.errors?.required && id.dirty" class="error">
+        Passenger ID is required
+      </div>
     </div>
 
     <hr />
@@ -47,14 +65,9 @@ import { Baggage } from '../../models/baggage.interface'
           {{ item.value }}
         </option>
       </select>
-      <select name="baggage" [ngModel]="detail?.baggage">
-        <option *ngFor="let item of baggage" [ngValue]="item.key">
-          {{ item.value }}
-        </option>
-      </select>
     </div>
-
-    {{ form.value | json }}
+    <div>Valid: {{ form.valid | json }}</div>
+    <div>Invalid: {{ form.invalid | json }}</div>
   </form>`,
 })
 export class PassengerFormComponent {
